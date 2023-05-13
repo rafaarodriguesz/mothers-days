ff3d7450c0bdd16ba4e9c2a2b8b99fd4ef0a65cc
Rafael <rr65887@gmail.com>
2023-05-12 22:58:39 -0300

    enviando projeto final

---
+++ js/index.js
@@ -0,0 +1,11 @@
+function fadeIn() {
+
+    const imgDiv = document.getElementById("img-container");
+    imgDiv.style.opacity = "1";
+}
+
+function fadeOut(){
+    
+    const imgDiv = document.getElementById("img-container");
+    imgDiv.style.opacity = "0";
+}
